a6bbf65c42ea539bf31f07ab2e067d9d344f9401
Henry Pan <henrypan917@gmail.com>
2015-08-17 21:57:34 -0400

    Sorting on dictionaries

--- cook_book_1.py
+++ cook_book_1.py
@@ -50,3 +50,50 @@ def deduplicate2(items, key=None):
 print set(a)  # this approach doesn't preserve any kind of ordering
 
 
+# Naming a slice
+record = '....................100          .......513.25     ..........'
+SHARES = slice(20, 32)
+PRICE = slice(40, 48)
+
+print int(record[SHARES]) * float(record[PRICE])
+
+a = slice(10, 50, 2)
+print a.start, a.stop, a.step
+
+s = 'HelloWorld'
+print a.indices(len(s))
+for i in range(*a.indices(len(s))):
+    print i
+    print(s[i])
+
+# Determine the Most Frequently Occurring Items in a Sequence
+words = [
+   'look', 'into', 'my', 'eyes', 'look', 'into', 'my', 'eyes',
+   'the', 'eyes', 'the', 'eyes', 'the', 'eyes', 'not', 'around', 'the',
+   'eyes', "don't", 'look', 'around', 'the', 'eyes', 'look', 'into',
+   'my', 'eyes', "you're", 'under'
+]
+
+from collections import Counter
+word_counts = Counter(words)
+top_three = word_counts.most_common(3)
+print(top_three)
+print word_counts['under']
+
+
+# Sorting a List of Dictionaries by a Common Key
+rows = [
+    {'fname': 'Brian', 'lname': 'Jones', 'uid': 1003},
+    {'fname': 'David', 'lname': 'Beazley', 'uid': 1002},
+    {'fname': 'John', 'lname': 'Cleese', 'uid': 1001},
+    {'fname': 'Big', 'lname': 'Jones', 'uid': 1004}
+]
+
+from operator import itemgetter
+
+rows_by_fname = sorted(rows, key=itemgetter('fname'))
+rows_by_uid = sorted(rows, key=itemgetter('uid'))
+
+print rows_by_fname
+print rows_by_uid
+
